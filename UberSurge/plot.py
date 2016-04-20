5bd7f350bba4e8de29848790f752260114121eeb
Kan Zhu <j20120307@gmail.com>
2016-04-20 01:37:06 -0400

    fix bugs

--- UberSurge/plot.py
+++ UberSurge/plot.py
@@ -10,7 +10,7 @@ with open('../data/output.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     # print spamreader
     for row in spamreader:
-    	if row[4] == "Goldman Sachs":
+    	if row[4] == "Google":
     		gs.append([row[0], row[1]])
 
 gs = np.asmatrix(gs)
@@ -18,13 +18,14 @@ gs = np.asmatrix(gs)
 for item in gs:
 	item[0,0] = datetime.datetime.fromtimestamp(float(item[0,0])).strftime("%H")
 
-idx = range(12,25)
-count = np.zeros(13)
-total_surge = np.zeros(13)
+idx = range(1,25)
+print idx
+count = np.zeros(24)
+total_surge = np.zeros(24)
 
 for item in gs:
-	count[int(item[0,0])-13] += 1
-	total_surge[int(item[0,0])-13] += float(item[0,1])
+	count[int(item[0,0])] += 1
+	total_surge[int(item[0,0])] += float(item[0,1])
 
 avg_surge = total_surge/count
 # for consistency test
@@ -32,7 +33,7 @@ avg_surge[np.isnan(avg_surge)] = np.random.uniform(1,1.5)
 # plt.plot(idx, avg_surge)
 # plt.show()
 
-result=np.zeros((13,2))
+result=np.zeros((24,2))
 result[:,0]=idx
 result[:,1]=avg_surge
 
@@ -42,8 +43,8 @@ with open('data.tsv', 'w') as tsvfile:
     writer.writerow(["time", "surge"])
     for record in result:
         data = []
-        data.append("[%s,"%record[0])
-        data.append("%s],"%record[1])
-        # "[%s,%s],"%(record[0], record[1])
-        print data
-        writer.writerow(data)
+        if record[0]>= 12:
+            data.append("[%s,"%record[0])
+            data.append("%s],"%record[1])
+            # "[%s,%s],"%(record[0], record[1])
+            writer.writerow(data)
