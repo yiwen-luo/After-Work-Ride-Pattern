8831d495f421b6181b1f30f49f11dd87bd0a5442
Kan Zhu <j20120307@gmail.com>
2016-05-02 13:41:21 -0400

    update README.md

--- rides_data/data_process.py
+++ rides_data/data_process.py
@@ -1,14 +1,18 @@
 import csv
 
+# get data from local csv file
 f = open("yellow_tripdata_2015-04.csv", "rb")
 
+# set filter date and time period for further analysis
 date = "2015-04-01"
 pick_time_start = "18:00:00"
 pick_time_end = "18:01:00"
 
 rows = csv.reader(f, delimiter=' ', quotechar='|')
+# std output the data into console with certain format
 for i, row_raw in enumerate(rows):
     if i > 0:
         row = row_raw[0].split(",") + row_raw[1].split(",") + row_raw[2].split(",")
+        # select data which is interested
         if row[1] == row[3] == date and pick_time_start <= row[2] <= pick_time_end:
             print row
--- uber_surge/mapquest.py
+++ uber_surge/mapquest.py
@@ -1,13 +1,14 @@
 import requests
 import ConfigParser
 
+# this is for temporary use. furthur use is incorporated in the incident folder
 
-def getBoundingBox(latitude, longitude, milesFromCenter):  
+def getBoundingBox(latitude, longitude, milesFromCenter):
     # format parameter string for the access of bouding box
     # given lat, lon and radius in miles, return the bounding box of square size
     approxLatDegreeMiles = 69
     approxLonDegreeMiles = 53
-  
+
     #calculate upper left
     ulLat = latitude + float(milesFromCenter) / float(approxLatDegreeMiles)
     ulLon = longitude + float(milesFromCenter) / float(approxLonDegreeMiles)
@@ -26,10 +27,9 @@ config.read('../config.cfg')
 
 key = config.get('mapquest_api', 'key')
 
-# test for Goldman Sachs' building 
-# get construction and incident data 
+# test for Goldman Sachs' building
+# get construction and incident data
 response = requests.get(url = "http://www.mapquestapi.com/traffic/v2/incidents?key=" + key + "&boundingBox=" + getBoundingBox(40.715082, -74.014390, 1.5) + "&filters=construction,incidents&inFormat=kvp&outFormat=json")
 data = response.json()
 
 print data["incidents"][0]
-
